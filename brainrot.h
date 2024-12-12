ae470b693579e8835c351c762d7bcaa7231fca9b
Sachin7717 <sachindhimann71@gmail.com>
2024-12-12 12:25:42 +0530

    header file for brainrot lang

---
+++ brainrot.h
@@ -0,0 +1,14 @@
+#ifndef brainrot
+#define brainrot
+#include<bits/stdc++.h>
+using namespace std;
+
+#define yapp cout
+typedef int mew;
+#define edge if
+#define goon else
+#define bust for
+#define rizz cin  
+#define skibidi main
+
+#endif
