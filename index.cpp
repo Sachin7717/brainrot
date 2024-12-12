fc41f3da590366ed3e5c810909b8a5ba1bb07ce2
Sachin7717 <sachindhimann71@gmail.com>
2024-12-12 12:26:24 +0530

    implementation of brainrot

---
+++ index.cpp
@@ -0,0 +1,16 @@
+#include"brainrot.h"
+using namespace std;
+
+mew skibidi(){
+     mew edgingStreak, hours=10;
+
+    rizz >> edgingStreak ;
+    edge(edgingStreak>hours){
+        yapp<<"youre a mogger"<<endl;
+    }
+    edge(edgingStreak<hours){
+        yapp<<"youre not a mogger"<<endl;
+    }
+
+    return 0;
+}
